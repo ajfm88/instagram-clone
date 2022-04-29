8669dcb10d1f4da1791b4c20e632953b568feb88
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-04-29 09:04:56 -0700

    01. Install Nextjs and Tailwind CSS and create the homepage template

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Instagram Clone</title>
+        <meta
+          name="Instagram social media app clone"
+          content="An Instagram clone created using Next.js, TailWindCSS and Google Firebase"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  );
+}
