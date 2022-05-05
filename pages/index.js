6d77c4e51071183ec47f2aabf6754b80353f88d7
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-05-04 18:17:35 -0700

    06. Populate fake data using minifaker and create feed and story components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-50 min-h-screen">
       <Head>
         <title>Instagram Clone</title>
         <meta
@@ -12,12 +13,13 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       {/* Header */}
 
       <Header />
 
       {/* Feed */}
+
+      <Feed />
 
       {/* Modal */}
     </div>
