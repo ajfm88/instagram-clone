e5a55fdffef9db94df2935e4731cc08cdeb73132
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-05-08 10:26:52 -0700

    18. Modify mini profile and story components to include the session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,16 +1,20 @@
+import { useSession, signOut } from "next-auth/react";
 export default function MiniProfile() {
+  const { data: session } = useSession();
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px]"
-        src="https://pbs.twimg.com/profile_images/1514774759292092427/2cLRlw1__400x400.jpg"
+        src={session?.user.image}
         alt="user-image"
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">ajfm88</h2>
+        <h2 className="font-bold">{session?.user.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+        Sign out
+      </button>
     </div>
   );
 }
