77050c6b0a0d9fdd90399a36745f723a8361559c
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-05-11 08:44:30 -0700

    23. Get the post data from database and show it in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,35 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Posts() {
-  const posts = [
-    {
-      id: "1",
-      username: "ajfm88",
-      userImg:
-        "https://pbs.twimg.com/profile_images/1514774759292092427/2cLRlw1__400x400.jpg",
-      img: "https://images.unsplash.com/photo-1643818507403-a3ed10760d16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-      caption: "Nice picture",
-    },
-    {
-      id: "2",
-      username: "ajfoucault",
-      userImg:
-        "https://i.pinimg.com/280x280_RS/7e/59/19/7e5919713171e8a08bdcc68878de653d.jpg",
-      img: "https://images.unsplash.com/photo-1643806720662-f9bc01be4e83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      caption: "New picture from my city",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  });
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
